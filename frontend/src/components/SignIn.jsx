5d3b57393ad8607586c16f6f80c2beb477c10821
ayushipaladiya246892 <ayushipaladiya92@gmail.com>
2024-12-28 04:28:00 +0530

    sign in

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import "./SignIn.css";
-import logo from "../img/logo.png";
+import logo from "../images/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
 import { LoginContext } from "../context/LoginContext";
