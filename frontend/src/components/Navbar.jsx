cd577c9d8c0c426620a3d1e1c63d0831ad268a21
ayushipaladiya246892 <ayushipaladiya92@gmail.com>
2024-12-28 04:20:25 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
-import logo from '../images/logo.png'
-import "./Navbar.css"
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import logo from "../img/logo.png";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { LoginContext } from "../context/LoginContext";
 
-const Navbar = () => {
+export default function Navbar({ login }) {
+  const { setModalOpen } = useContext(LoginContext);
+  const loginStatus = () => {
+    const token = localStorage.getItem("jwt");
+    if (login || token) {
+      return [
+        <>
+          <Link to="/profile">
+            <li>Profile</li>
+          </Link>
+          <Link to="/createPost">Create Post</Link>
+          <Link style={{ marginLeft: "20px" }} to="/followingpost">
+            My Following
+          </Link>
+          <Link to={""}>
+            <button className="primaryBtn" onClick={() => setModalOpen(true)}>
+              Log Out
+            </button>
+          </Link>
+        </>,
+      ];
+    } else {
+      return [
+        <>
+          <Link to="/signup">
+            <li>SignUp</li>
+          </Link>
+          <Link to="/signin">
+            <li>SignIn</li>
+          </Link>
+        </>,
+      ];
+    }
+  };
 
   return (
-    <div className='navbar'>
-       <img src={logo} alt=''/>
-       <ul className='nav-menu'>
-
-        <Link to='/signup'>
-        <li>SignUp</li>
-        </Link>
-
-        <Link to='/signin'>
-        <li>SignIn</li>
-        </Link>
-
-        <Link to='/profile'>
-        <li>Profile</li>
-        </Link>
-
-        <Link to='/createpost'>
-        <li>Create Post </li>
-        </Link>
-
-        <Link to='/modal'>
-        <li>Logout </li>
-        </Link>
-        </ul>
+    <div className="navbar">
+      <img src={logo} alt="" />
+      <ul className="nav-menu">{loginStatus()}</ul>
     </div>
-  )
+  );
 }
-
-export default Navbar
