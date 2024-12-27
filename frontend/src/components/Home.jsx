428026cc3aebf731d78aed66bbde1a6cb2b53357
ayushipaladiya246892 <ayushipaladiya92@gmail.com>
2024-12-28 04:17:21 +0530

    home jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,87 +1,297 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import useNavigate
-import './Home.css';
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-  const [comment, setComment] = useState('');
-  const [likes, setLikes] = useState(1); // Initial likes count
+export default function Home() {
+  var picLink = "https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [comment, setComment] = useState("");
+  const [show, setShow] = useState(false);
+  const [item, setItem] = useState([]);
 
-  // Handle comment input change
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
+  // Toast functions
+  const notifyA = (msg) => toast.error(msg);
+  const notifyB = (msg) => toast.success(msg);
 
-  // Handle like button click
-  const handleLike = () => {
-    setLikes(likes + 1); // Increment like count
-    console.log('Likes:', likes + 1); // Print the updated like count to console
-  // Redirect to the /profile page
-  };
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+      navigate("./signup");
+    }
 
-  // Handle comment submission
-  const handleCommentSubmit = (e) => {
-    e.preventDefault(); // Prevent page reload on form submission
-    if (comment.trim()) {
-      console.log('Comment:', comment); // Print comment to console
-      setComment(''); // Clear the comment input field
-     // Redirect to the /profile page
+    // Fetching all posts
+    fetch("http://localhost:5000/allposts", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setData(result);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // to show and hide comments
+  const toggleComment = (posts) => {
+    if (show) {
+      setShow(false);
+    } else {
+      setShow(true);
+      setItem(posts);
     }
   };
 
+  const likePost = (id) => {
+    fetch("http://localhost:5000/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((posts) => {
+          if (posts._id == result._id) {
+            return result;
+          } else {
+            return posts;
+          }
+        });
+        setData(newData);
+        console.log(result);
+      });
+  };
+  const unlikePost = (id) => {
+    fetch("http://localhost:5000/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((posts) => {
+          if (posts._id == result._id) {
+            return result;
+          } else {
+            return posts;
+          }
+        });
+        setData(newData);
+        console.log(result);
+      });
+  };
+
+  // function to make comment
+  const makeComment = (text, id) => {
+    fetch("http://localhost:5000/comment", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        text: text,
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((posts) => {
+          if (posts._id == result._id) {
+            return result;
+          } else {
+            return posts;
+          }
+        });
+        setData(newData);
+        setComment("");
+        notifyB("Comment posted");
+        console.log(result);
+      });
+  };
+
   return (
-    <div>
+    <div className="home">
       {/* card */}
-      <div className="card">
-        {/* card header */}
-        <div className="card-header">
-          <div className="card-pic">
-            <img
-              src="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D"
-              alt=""
-            />
+      {data.map((posts) => {
+        return (
+          <div className="card">
+            {/* card header */}
+            <div className="card-header">
+              <div className="card-pic">
+                <img
+                  src={posts.postedBy.Photo ? posts.postedBy.Photo : picLink}
+                  alt=""
+                />
+              </div>
+              <h5>
+                <Link to={`/profile/${posts.postedBy._id}`}>
+                  {posts.postedBy.name}
+                </Link>
+              </h5>
+            </div>
+            {/* card image */}
+            <div className="card-image">
+              <img src={posts.photo} alt="" />
+            </div>
+
+            {/* card content */}
+            <div className="card-content">
+              {posts.likes.includes(
+                JSON.parse(localStorage.getItem("user"))._id
+              ) ? (
+                <span
+                  className="material-symbols-outlined material-symbols-outlined-red"
+                  onClick={() => {
+                    unlikePost(posts._id);
+                  }}
+                >
+                  favorite
+                </span>
+              ) : (
+                <span
+                  className="material-symbols-outlined"
+                  onClick={() => {
+                    likePost(posts._id);
+                  }}
+                >
+                  favorite
+                </span>
+              )}
+
+              <p>{posts.likes.length} Likes</p>
+              <p>{posts.body} </p>
+              <p
+                style={{ fontWeight: "bold", cursor: "pointer" }}
+                onClick={() => {
+                  toggleComment(posts);
+                }}
+              >
+                View all comments
+              </p>
+            </div>
+
+            {/* add Comment */}
+            <div className="add-comment">
+              <span className="material-symbols-outlined">mood</span>
+              <input
+                type="text"
+                placeholder="Add a comment"
+                value={comment}
+                onChange={(e) => {
+                  setComment(e.target.value);
+                }}
+              />
+              <button
+                className="comment"
+                onClick={() => {
+                  makeComment(comment, posts._id);
+                }}
+              >
+                Post
+              </button>
+            </div>
           </div>
-          <Link to='/profile'>
-          <h5>Rahir</h5>
-          </Link>
-        </div>
+        );
+      })}
 
-        {/* card image */}
-        <div className="card-image">
-          <img
-            src="https://plus.unsplash.com/premium_photo-1700838996810-eb6c0101902b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-          />
-        </div>
+      {/* show Comment */}
+      {show && (
+        <div className="showComment">
+          <div className="container">
+            <div className="postPic">
+              <img src={item.photo} alt="" />
+            </div>
+            <div className="details">
+              {/* card header */}
+              <div
+                className="card-header"
+                style={{ borderBottom: "1px solid #00000029" }}
+              >
+                <div className="card-pic">
+                  <img
+                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                    alt=""
+                  />
+                </div>
+                <h5>{item.postedBy.name}</h5>
+              </div>
 
-        {/* card content */}
-        <div className="card-content">
-          <span
-            className="material-symbols-outlined"
-            onClick={handleLike}
-            style={{ cursor: 'pointer' }}
+              {/* commentSection */}
+              <div
+                className="comment-section"
+                style={{ borderBottom: "1px solid #00000029" }}
+              >
+                {item.comments.map((comment) => {
+                  return (
+                    <p className="comm">
+                      <span
+                        className="commenter"
+                        style={{ fontWeight: "bolder" }}
+                      >
+                        {comment.postedBy.name}{" "}
+                      </span>
+                      <span className="commentText">{comment.comment}</span>
+                    </p>
+                  );
+                })}
+              </div>
+
+              {/* card content */}
+              <div className="card-content">
+                <p>{item.likes.length} Likes</p>
+                <p>{item.body}</p>
+              </div>
+
+              {/* add Comment */}
+              <div className="add-comment">
+                <span className="material-symbols-outlined">mood</span>
+                <input
+                  type="text"
+                  placeholder="Add a comment"
+                  value={comment}
+                  onChange={(e) => {
+                    setComment(e.target.value);
+                  }}
+                />
+                <button
+                  className="comment"
+                  onClick={() => {
+                    makeComment(comment, item._id);
+                    toggleComment();
+                  }}
+                >
+                  Post
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="close-comment"
+            onClick={() => {
+              toggleComment();
+            }}
           >
-            favorite
-          </span>
-          <p>{likes} Like{likes > 1 ? 's' : ''}</p>
-          <p>This is amazing</p>
+            <span className="material-symbols-outlined material-symbols-outlined-comment">
+              close
+            </span>
+          </div>
         </div>
-
-        {/* add comment */}
-        <div className="add-comment">
-          <span className="material-symbols-outlined">mood</span>
-          <input
-            type="text"
-            placeholder="Add a Comment"
-            value={comment}
-            onChange={handleCommentChange}
-          />
-          <button className="comment" onClick={handleCommentSubmit}>
-            Post
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
-};
-
-export default Home;
+}
