49ddec1717ca5d563084fcd5dcf89b0514e9ce0e
ayushipaladiya246892 <ayushipaladiya92@gmail.com>
2024-12-28 04:18:33 +0530

    modals

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,44 +1,38 @@
 import React from "react";
 import { RiCloseLine } from "react-icons/ri";
-import { useNavigate } from "react-router-dom"; // Import useNavigate for redirection
 import "./Modal.css";
+import { useNavigate } from "react-router-dom";
 
-const Modal = () => {
+export default function Modal({ setModalOpen }) {
   const navigate = useNavigate();
-
-  const handleLogout = () => {
-    // Clear any authentication tokens or session data
-    localStorage.removeItem("authToken"); // Example: Clear token from localStorage
-    // Redirect to sign-in page
-    navigate("/signin");
-  };
-
-  const handleClose = () => {
-    // Add logic to close the modal if needed
-    console.log("Modal closed");
-    navigate("/profile")
-  };
-
   return (
-    <div className="darkBg">
+    <div className="darkBg" onClick={() => setModalOpen(false)}>
       <div className="centered">
         <div className="modal">
-          {/* Modal Header */}
+          {/* modal header */}
           <div className="modalHeader">
             <h5 className="heading">Confirm</h5>
           </div>
-          <button className="closeBtn" onClick={handleClose}>
-            <RiCloseLine />
+          <button className="closeBtn" onClick={() => setModalOpen(false)}>
+            <RiCloseLine></RiCloseLine>
           </button>
-          {/* Modal Content */}
-          <div className="modalContent">Are you sure you want to log out?</div>
+          {/* modal content */}
+          <div className="modalContent">Are you really want to log Out ?</div>
           <div className="modalActions">
             <div className="actionsContainer">
-              <button className="logOutBtn" onClick={handleLogout}>
+              <button
+                className="logOutBtn"
+                onClick={() => {
+                  setModalOpen(false);
+                  localStorage.clear();
+                  navigate("./signin");
+                }}
+              >
                 Log Out
               </button>
-              <button className="cancelBtn" onClick={handleClose}>
-                Cancel
+
+              <button className="cancelBtn" onClick={() => setModalOpen(false)}>
+                cancel
               </button>
             </div>
           </div>
@@ -46,6 +40,4 @@ const Modal = () => {
       </div>
     </div>
   );
-};
-
-export default Modal;
+}
